483d30fdfa60f94bddfdd91014dde42ad92fb237
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-05-30 17:46:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 function Changes(){
-	document.getElementById("status").innerHTML = "<h1>Enter the Metaverse</h1>"
+	document.getElementById("status").innerHTML = "<h1>Entered Metaverse</h1>"
 }
